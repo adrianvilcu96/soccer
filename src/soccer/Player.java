688c498a6cbdc2e04b10da43ede91913c952de12
adrianvilcu96 <37113811+adrianvilcu96@users.noreply.github.com>
2020-01-13 17:01:48 +0100

    Update Player.java

--- src/soccer/Player.java
+++ src/soccer/Player.java
@@ -12,7 +12,7 @@ import javax.persistence.Table;
 import com.fasterxml.jackson.annotation.JsonBackReference;
 
 @Entity
-@Table(name=players)
+@Table(name="players")
 public class Player implements Serializable {
 	
 	static final long serialVersionUID = 3;
